134542cfaa9bb119c453f35a3766ab2b503412dd
Yethi <yethinder@gmail.com>
2026-05-30 19:41:58 -0400

    Initial Commit

---
+++ Doc Narrator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Doc Narrator
+//
+//  Created by AIM on 5/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Doc Narrator/Doc_NarratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doc_NarratorApp.swift
+//  Doc Narrator
+//
+//  Created by AIM on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct Doc_NarratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
